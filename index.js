b989a11dcedf6ffe9388b928f9d71907f0f2173c
mdimran1409036 <mdimran1409036@gmail.com>
2021-11-11 00:22:32 +0600

    user post api added

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000
+
+// connect to mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.krune.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("nokshi");
+        const usersCollection = database.collection("users");
+
+        console.log('database created');
+
+        app.post('/users', async(req,res)=>{
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.json(result)
+        })
+       
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+//middlewares
+app.use(cors())
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
